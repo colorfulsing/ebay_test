0512a5407e671fb9ae1b704fc1aa85e20e59ca3a
ER <colorfulsing@gmail.com>
2020-08-15 19:39:31 -0600

    Get product status

--- parsers/products.rb
+++ parsers/products.rb
@@ -1,7 +1,11 @@
 html = Nokogiri::HTML(content)
 html.css('.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card [itemscope="itemscope"]').each do |product|
-  outputs << {
-    name: product.css('h3').text.strip,
-    price: product.css('[itemprop="price"]').text.strip
-  }
+  name = product.css('h3').text.strip
+  if name =~ /black/i
+    url = product.css('[itemprop="url"]')
+    pages << {
+      url: url,
+      page_type: 'single_product'
+    }
+  end
 end
---
+++ parsers/single_product.rb
@@ -0,0 +1,11 @@
+html = Nokogiri::HTML(content)
+
+name = html.css('[itemprop="name"]').text.strip
+status = html.css('#vi-itm-cond').text.strip
+price = product.css('#prcIsum').text.strip.match(/([0-9]|\.)+/).to_s
+
+outputs << {
+  name: name,
+  status: status,
+  price: price.to_f
+}
