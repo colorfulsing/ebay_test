3b43e0358e61af69a0231afc4519f0d2159d74d2
ER <colorfulsing@gmail.com>
2020-08-15 19:41:53 -0600

    Fix

--- parsers/products.rb
+++ parsers/products.rb
@@ -2,7 +2,7 @@ html = Nokogiri::HTML(content)
 html.css('.ebayui-dne-item-featured-card.ebayui-dne-item-featured-card [itemscope="itemscope"]').each do |product|
   name = product.css('h3').text.strip
   if name =~ /black/i
-    url = product.css('[itemprop="url"]')
+    url = product.css('[itemprop="url"]').attr('href').text.strip
     pages << {
       url: url,
       page_type: 'single_product'
