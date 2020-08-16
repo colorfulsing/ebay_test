f04fded779b60e3a1b08c4c504a7e639bd5f1435
ER <colorfulsing@gmail.com>
2020-08-15 19:47:16 -0600

    Fix

--- parsers/single_product.rb
+++ parsers/single_product.rb
@@ -2,7 +2,7 @@ html = Nokogiri::HTML(content)
 
 name = html.css('[itemprop="name"]').text.strip
 status = html.css('#vi-itm-cond').text.strip
-price = product.css('#prcIsum').text.strip.match(/([0-9]|\.)+/).to_s
+price = html.css('#prcIsum').text.strip.match(/([0-9]|\.)+/).to_s
 
 outputs << {
   name: name,
